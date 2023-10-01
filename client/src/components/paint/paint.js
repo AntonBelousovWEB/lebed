f640053e3b5839eed4e3e395dfbd96ec51d2618f
BoRoDaTeNkiY <polpoltrop228@gmail.com>
2023-10-01 22:43:54 +0300

    ui, donate

--- client/src/components/paint/paint.js
+++ client/src/components/paint/paint.js
@@ -86,6 +86,13 @@ function Paint() {
           height={"1000px"}
         />
       </div>
+      <div className='sidebar'>
+        <div className='chat scroll'>
+          <h2 className='title_chat'>LEBED CHAT</h2>
+          <input className='input_text_chat' type='text' placeholder='type text' />
+          <svg className='send' xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="none"><path d="M.5 1.163A1 1 0 0 1 1.97.28l12.868 6.837a1 1 0 0 1 0 1.766L1.969 15.72A1 1 0 0 1 .5 14.836V10.33a1 1 0 0 1 .816-.983L8.5 8 1.316 6.653A1 1 0 0 1 .5 5.67V1.163Z" fill="currentColor"></path></svg>
+        </div>
+      </div>
     </div>
   );
 }
